8233d991dbbb9aa1e13648d8431d5be2f1e1d9bf
Odzao Esther Lardze <99372812+estherlardze@users.noreply.github.com>
2023-09-21 07:56:31 +0000

    Update firebase.js

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_FIREBASE_API_KEY,
   authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_FIREBASE_MESS_SENDER_ID,
-  appId: ""
+  storageBucket: process.env.REACT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_FIREBASE_MESS_SENDER_ID,
+  appId: process.env.REACT_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
