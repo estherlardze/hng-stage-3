a26a58b32710b1e24d19b5176ffeb79490e9bb78
estherlardze <estherlardze@gmail.com>
2023-09-21 23:51:44 +0100

    final commit

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { images } from '../pages/index';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 
 const Gallery = ({search, filteredItems}) => {
+  const [characters, setCharacters] = useState(finalSpaceCharacters)
   return (
     <DragDropContext onDragEnd={(result) => console.log(result)} >
-    <div className='flex flex-wrap gap-[20px] mx-[30px] md:mx-[auto]'>
-   
-        {!search ? images.map((item) => (
-           <Droppable droppableId={item.id}>
+       <Droppable droppableId="characters">
+         {(provided) => (
+       <div className='flex flex-wrap gap-[20px] mx-[30px] md:mx-[auto]' {...provided.droppableProps} ref={provided.innerRef}>
+          {images.map((item, index) => (
+            <Draggable key={item.id} draggableId={item.id}index={index} >
               {(provided) => (
-            <div key={item.id} className='relative' {...provided.droppableProps} ref={provided.innerRef}>  
-               <img src={item.image} alt={item.tag} className=' w-[94%] md:w-[300px] h-[350px] rounded-md'/> 
-               <button className='absolute top-[20px] right-[20px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>{item.tag}</button>
-            </div>
+             <div  className='relative' {...provided.droppableProps} {...provided.dragHandleProps} ref={provided.innerRef}>  
+               <img src={item.image} alt={item.tag} className=' w-[250] md:w-[300px] h-[350px] rounded-md'/> 
+              <button className='absolute top-[20px] right-[20px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>{item.tag}</button>
+             </div>
               )}
-            </Droppable>
-            ))
-            :
-            filteredItems.map((item) => (
-                <Droppable droppableId={item.id}>
-                    {(provided) => (
-                <div key={item.id} className='relative' {...provided.droppableProps} ref={provided.innerRef}>  
-                   <img src={item.image} alt={item.tag} className='w-[300px] h-[350px] rounded-md'/> 
-                   <button className='absolute top-[10px] right-[10px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>{item.tag}</button>
-                </div>
-                )}
-                </Droppable>
-            ))
-        }
-         </div>
-     </DragDropContext>
-  )
-}
-
+             </Draggable>
+          ))}
+      {provided.placeholder}
+       </div>
+         )}
+        </Droppable>
+      </DragDropContext>
+            
+        )}
+           
+            {/* // filteredItems.map((item) => (
+            //     <Droppable droppableId={item.id}>
+            //         {(provided) => (
+            //     <div key={item.id} className='relative' {...provided.droppableProps} ref={provided.innerRef}>  
+            //        <img src={item.image} alt={item.tag} className='w-[300px] h-[350px] rounded-md'/> 
+            //        <button className='absolute top-[10px] right-[10px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>{item.tag}</button>
+            //     </div>
+            //     )} */}
+              
+            
 export default Gallery
