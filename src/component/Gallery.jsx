f080797be31d239b0cfb3528dfe79cdd09716cc2
estherlardze <estherlardze@gmail.com>
2023-09-23 12:26:04 +0100

    final commit

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -40,7 +40,7 @@ const Gallery = ({ search, isLoading, setIsLoading}) => {
         <img
           src={item.image}
           alt={item.tag}
-          className='w-[250px] md:w-[300px] h-[350px] rounded-md'
+          className='w-[250px] md:w-[300px] h-[350px] rounded-md cursor-pointer'
         />
         <button className='absolute top-[20px] right-[20px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>
           {item.tag}
