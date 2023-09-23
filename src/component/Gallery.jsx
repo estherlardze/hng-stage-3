b11b4dbafcd336fee628ebb81df9b7f440ed20be
estherlardze <estherlardze@gmail.com>
2023-09-23 12:13:48 +0100

    aded dnd

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,61 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { images } from '../pages/index';
-import ImageCard  from './ImageCard';
-import Loader from './Loader'
-import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
+import React, { useState } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import ImageCard from './ImageCard'
+import { images} from '../pages/index';
 
+const Gallery = ({ search, isLoading, setIsLoading}) => {
 
-const Gallery = ({search, isLoading, setIsLoading}) => {
-  const [image, setImage] = useState(images)
+  const [image, setImage] = useState(images);
 
- 
   useEffect(() => {
     setIsLoading(true);
     setTimeout(() => {
       setIsLoading(false);
-    }, 2000); 
+    }, 2000);
   }, []);
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
-
-    const items = Array.from(image);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setImage(items);
-  }
+  const handleMoveImage = (dragIndex, hoverIndex) => {
+    const draggedImage = image[dragIndex];
+    const newImageArray = [...image];
+    newImageArray.splice(dragIndex, 1);
+    newImageArray.splice(hoverIndex, 0, draggedImage);
+    setImage(newImageArray);
+  };
+  const Image = ({ item, index }) => {
+    const [, ref] = useDrag({
+      type: 'IMAGE',
+      item: { index },
+    });
+    const [, drop] = useDrop({
+      accept: 'IMAGE',
+      hover: (draggedItem) => {
+        if (draggedItem.index !== index) {
+          handleMoveImage(draggedItem.index, index);
+          draggedItem.index = index;
+        }
+      },
+    });
+    return (
+      <div ref={(node) => ref(drop(node))} className='relative'>
+        <img
+          src={item.image}
+          alt={item.tag}
+          className='w-[250px] md:w-[300px] h-[350px] rounded-md'
+        />
+        <button className='absolute top-[20px] right-[20px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>
+          {item.tag}
+        </button>
+      </div>
+    );
+  };
   return (
-    <div>
-      {isLoading ? (<Loader />)
-     :
-   ( <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="characters">
-      {(provided) => (
-       <div className='flex flex-wrap gap-[20px] mx-[30px] md:mx-[auto]' {...provided.droppableProps}
-        ref={provided.innerRef}>
-
-         {!search && images.map((item, index) => (
-         
-           <Draggable  key={item.id} draggableId={item.id.toString()} index={index}>
-            
-            {(provided) => (
-            //  <ImageCard item={item} {...provided.droppableProps}
-            //   ref={provided.innerRef} {...provided.dragHandleProps} />
-            <div className='relative' {...provided.droppableProps} ref={provided.innerRef} {...provided.dragHandleProps}> 
-
-            <img src={item.image} alt={item.tag} className=' w-[250] md:w-[300px] h-[350px] rounded-md'/> 
-            <button className='absolute top-[20px] right-[20px] bg-red-400 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded-xl'>{item.tag}</button>
-           </div>
-            )}
-          </Draggable>
-           ))}
-           {provided.placeholder}
-        </div>
-        )}
-       </Droppable>
-    </DragDropContext> 
-  )}
-  </div>
- ) } 
-export default Gallery
+    <div className='flex flex-wrap gap-[20px] mx-[30px] md:mx-[auto]'>
+      {image.map((item, index) => (
+        <Image key={item.id} item={item} index={index} />
+      ))}
+    </div>
+  );
+};
+export default Gallery;
--- src/pages/index.js
+++ src/pages/index.js
@@ -32,132 +32,132 @@ import photo26 from '../assets/photo-26.jpg'
 
 export const images = [
     {
-        id: 1,
+        id: '1',
         image: photo1,
         tag: 'drawing'
     },
     {
-        id: 2,
+        id: '2',
         image: photo2,
         tag: 'lady'
     },
      {
-        id: 3,
+        id: '3',
         image: photo3,
         tag: 'model'
     },
     {
-        id: 4,
+        id: '4',
         image: photo4,
         tag: 'walking'
     },
     {
-        id: 5,
+        id: '5',
         image: photo5,
         tag: 'classy'
     },
     {
-        id: 6,
+        id: '6',
         image: photo6,
         tag: 'jeep'
     },
     {
-        id: 7,
+        id: '7',
         image: photo7,
         tag: 'lady'
     },
      {
-        id: 8,
+        id: '8',
         image: photo8,
         tag: 'biking'
     },
      {
-        id: 9,
+        id: '9',
         image: photo9,
         tag: 'profile'
     },
     {
-        id: 10,
+        id: '10',
         image: photo10,
         tag: 'wedding'
     },
     {
-        id: 11,
+        id: '11',
         image: photo11,
         tag: 'learning'
     },
     {
-        id: 12,
+        id: '12',
         image: photo12,
         tag: 'birthday'
     },
      {
-        id: 13,
+        id: '13',
         image: photo13,
         tag: 'sampling'
     },
     {
-        id: 14,
+        id: '14',
         image: photo14,
         tag: 'cat'
     },
      {
-        id: 15,
+        id: '15',
         image: photo15,
         tag: 'Cutie'
     },
     {
-        id: 16,
+        id: '16',
         image: photo16,
         tag: 'dark'
     },
      {
-        id: 17,
+        id: '17',
         image: photo17,
         tag: 'mountain'
     },
     {
-        id: 18,
+        id: '18',
         image: photo18,
         tag: 'date'
     },
     {
-        id: 19,
+        id: '19',
         image: photo19,
         tag: 'dinner'
     },
      {
-        id: 20,
+        id: '20',
         image: photo20,
         tag: 'meeting'
     },
     {
-        id: 21,
+        id: '21',
         image: photo21,
         tag: 'analysis'
     },
     {
-        id: 22,
+        id: '22',
         image: photo22,
         tag: 'seminer'
     },
     {
-        id: 23,
+        id: '23',
         image: photo23,
         tag: 'friends'
     },
     {
-        id: 24,
+        id: '24',
         image: photo24,
         tag: 'meeting'
     },
     {
-        id: 25,
+        id: '25',
         image: photo25,
         tag: 'analysis'
     },
     {
-        id: 26,
+        id: '26',
         image: photo26,
         tag: 'party'
     }
